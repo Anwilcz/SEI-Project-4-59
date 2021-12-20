8baae8934320ccff24366bc1bd15925055ac84dd
Anna Wilczynska <anna.wilcz@outlook.com>
2021-12-20 21:57:40 +0100

    Ready to deploy

--- tool/admin.py
+++ tool/admin.py
@@ -1,30 +1,30 @@
 from django.contrib import admin
 from .models import *
-# from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ImportExportModelAdmin
 
 
 # Register your models here.
-# class ToolAdmin(ImportExportModelAdmin):
-#   pass
+class ToolAdmin(ImportExportModelAdmin):
+  pass
 
-admin.site.register(Tool)
+admin.site.register(Tool, ToolAdmin)
 
-# class WantsToWorkWithCompanySizeAdmin(ImportExportModelAdmin):
-#   pass
+class WantsToWorkWithCompanySizeAdmin(ImportExportModelAdmin):
+  pass
 
-admin.site.register(WantsToWorkWithCompanySize)
+admin.site.register(WantsToWorkWithCompanySize, WantsToWorkWithCompanySizeAdmin)
 
-# class WorkedWithCompanySizeAdmin(ImportExportModelAdmin):
-#   pass
+class WorkedWithCompanySizeAdmin(ImportExportModelAdmin):
+  pass
 
-admin.site.register(WorkedWithCompanySize)
+admin.site.register(WorkedWithCompanySize, WorkedWithCompanySizeAdmin)
 
-# class WantsToWorkWithCountryAdmin(ImportExportModelAdmin):
-#   pass
+class WantsToWorkWithCountryAdmin(ImportExportModelAdmin):
+  pass
 
-admin.site.register(WantsToWorkWithCountry)
+admin.site.register(WantsToWorkWithCountry, WantsToWorkWithCountryAdmin)
 
-# class WorkedWithCountryAdmin(ImportExportModelAdmin):
-#   pass
+class WorkedWithCountryAdmin(ImportExportModelAdmin):
+  pass
 
-admin.site.register(WorkedWithCountry)
+admin.site.register(WorkedWithCountry, WorkedWithCountryAdmin)
