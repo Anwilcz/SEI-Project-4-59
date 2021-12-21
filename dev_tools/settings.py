0e4e8e8a122c9c0c1bf5d48518ba28b3a187f0af
Anna Wilczynska <anna.wilcz@outlook.com>
2021-12-21 01:51:40 +0100

    Changing images url

--- dev_tools/settings.py
+++ dev_tools/settings.py
@@ -137,11 +137,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'client', 'build', 'media')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'client', 'build', 'static', 'media')
 
 MEDIA_URL = '/media/'
 
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
